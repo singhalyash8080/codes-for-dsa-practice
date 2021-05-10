7dce1fe0f04e4721bce3189e27946b953c35402f
Yash Singhal <yash.singhal2019@vitstudent.ac.in>
2021-05-10 20:33:39 +0530

    added more codes

---
+++ binaryTree_operations.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+
+void yashio();
+
+class node{
+public:
+    int data;
+
+    node *left;
+    node *right;
+
+    node(int d){
+        data = d;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+node *buildTree(){
+
+    int d;
+    cin>>d;
+
+    if(d==-1){
+        return NULL;
+    }
+
+    node *root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+
+    return root;
+}
+
+void printTreePreorder(node *root){
+
+    if(root==NULL){
+        return;
+    }
+
+    cout<<root->data<<" ";
+    printTreePreorder(root->left);
+    printTreePreorder(root->right); 
+}
+
+void printTreeInorder(node *root){
+
+    if(root==NULL){
+        return;
+    }
+    printTreeInorder(root->left);
+    cout<<root->data<<" ";
+    printTreeInorder(root->right);
+}
+
+void printTreePostorder(node *root){
+
+    if(root==NULL){
+        return;
+    }
+    printTreePostorder(root->left);
+    printTreePostorder(root->right);
+    cout<<root->data<<" ";
+}
+
+void solve()
+{
+    node *root = buildTree();
+
+    printTreePreorder(root);
+    cout<<endl;
+    printTreeInorder(root);
+    cout<<endl;
+    printTreePostorder(root);
+
+}
+
+signed main()
+{
+    yashio();
+    int T = 1;
+    //cin>>T;
+    while(T--)
+    {
+        solve();
+    }
+}
+
+void yashio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input999.txt", "r", stdin);
+    freopen("output999.txt", "w", stdout);
+#endif
+}
